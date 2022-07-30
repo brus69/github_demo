81b7fba26531b80edd66f636d5a1640bdfc2208a
brus69 <96365067+brus69@users.noreply.github.com>
2022-07-30 14:59:50 +0300

    Добавил функцию деление

--- first/nums.py
+++ first/nums.py
@@ -6,8 +6,11 @@ def mult(a,b):
 
 def minus(a , b):
     return a - b
+def del_s(a,b):
+    return a / b
 
 if __name__ == '__main__':
     print(plus(2,2))
     print(minus(3, 1))
     print(mult(10,10))
+    print(del_s(6,2))
